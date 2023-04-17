c54e2b51f8e9b75eb21caeddfab20268106d1b34
Raymond Ma <maraymond@yahoo.com>
2023-04-17 03:19:04 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+CLIENT_SECRET_FILE = "PATH_TO_SECRET_FILE_FOR_YOUTUBE_API"
+
+FAVORITE_TEAMS = ['warriors','lakers','suns', 'nuggets', '76']
+
+FAVORITE_PLAYERS = ['booker', 'giannis', 'Shai', 'anthony Davis', 'stephen curry', 'klay']
+
+# Set score for player to be considered high scorer
+HIGH_SCORE = 50
